414018ca60b91f82bdb9c3df31114c3fd1d04796
Desh Iyer <desh.iyer@gmail.com>
2022-08-27 16:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
-from flask import Flask, render_template, url_for
-# Import sqlalchemy
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-# Add config for sqlalchemy
-# Telling our app where the database is
-# '///' is a relative path and '////' is an absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-# Initialise the database
 db = SQLAlchemy(app)
 
-# Create the database model
 class Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     
-    # This is always set automatically and never manually since it has a default field
     dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
     
-    # Creating a repr function to represent each task (object) as a string
     def __repr__(self):
         retString = 'Task #{}'.format(self.id)
         
         return retString
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('2-template-inheritance-index.html')
+    # If the submit button on the form is pressed
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
