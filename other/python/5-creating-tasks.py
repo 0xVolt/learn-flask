57b139ad98d557370ac81250193cc7edf5dc6648
Desh Iyer <desh.iyer@gmail.com>
2022-08-28 00:07:24 +0530

    Logging .css files

---
+++ other/python/5-creating-tasks.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+class Model(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        retString = 'Task #{}'.format(self.id)
+        
+        return retString
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    # If the submit button on the form is pressed
+    if request.method == 'POST':
+        # Pass in the id of the input to extract
+        taskContent = request.form['content']
+        
+        # Create an object of the table having content as the extracted content above
+        newTask = Model(content=taskContent)
+        
+        # Pushing the new task created to the database
+        try:
+            # Look familiar? This code pushes data to our database
+            db.session.add(newTask)
+            db.session.commit()
+            
+            # Remember to import redirect back to the index page
+            return redirect('/')
+        except:
+            # This should never fail ideally
+            return 'There was an issue adding your task to the database.'
+    else:
+        # This looks at all the database contents and returns them in the order of their creation
+        tasks = Model.query.order_by(Model.dateCreated).all()
+        # tasks = Model.query.order_by(Model.dateCreated).first()
+        
+        # Passing the tasks variable into the render_template() function
+        return render_template('index.html', tasks=tasks)
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
